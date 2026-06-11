cba0070126172601120b9748fe2b1716e6f1559d
Brooke F <brooke.floyd10@gmail.com>
2026-06-11 03:29:00 -0500

    added more styling, spaced out the fun facts heading

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -4,10 +4,10 @@ let funFactsArr = [
     "I sing in a folk band",
     "I recently achieved a childhood goal of, learning to juggle",
     "I have a cat named Harriet, after Harriet The Spy",
-    "<img src='images/Harriet.jpg' width='90%' alt='My cat harriet snoozing while cuddling a playstation controller.'/>",
+    "<img src='images/Harriet.jpg' width='100%' alt='My cat harriet snoozing while cuddling a playstation controller.'/>",
     "I love a DIY home renovation project! I have tackled everything from structural changes to full room and bathroom remodels",
     "I have a dog named skittles",
-    "<img src='images/skits.jpg' width='90%' alt='My dog sitting in the driver seat of my car.'/>",
+    "<img src='images/skits.jpg' width='100%' alt='My dog sitting in the driver seat of my car.'/>",
     "I have a green thumb… but only for plants. Flowers and I are not on speaking terms."
 ];
 const funfactId = document.getElementById("fun-fact");
