d85a3fb3a00860ae37e287eeaab7d19ae899e824
Brooke F <brooke.floyd10@gmail.com>
2026-06-11 14:28:53 -0500

    completed more styling and restructuring in the aside and form section

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,14 +1,31 @@
 
 
 let funFactsArr = [
-    "I sing in a folk band",
-    "I recently achieved a childhood goal of, learning to juggle",
-    "I have a cat named Harriet, after Harriet The Spy",
-    "<img src='images/Harriet.jpg' width='100%' alt='My cat harriet snoozing while cuddling a playstation controller.'/>",
-    "I love a DIY home renovation project! I have tackled everything from structural changes to full room and bathroom remodels",
-    "I have a dog named skittles",
-    "<img src='images/skits.jpg' width='100%' alt='My dog sitting in the driver seat of my car.'/>",
-    "I have a green thumb… but only for plants. Flowers and I are not on speaking terms."
+  "I sing in a folk band",
+
+  "<p>I love DIY home renovation projects of all types.</p><br>" +
+  "<img src='images/kitchen.png' width='100%' alt='Kitchen remodel featuring a picture window addition.'/><br>" +
+  "<p>I opened up my kitchen by adding a picture window.</p>",
+
+  "I recently learned to juggle and fulfilled a childhood goal.",
+
+  "I have been skydiving.",
+
+  "<p>Meet Harriet!</p><br>" +
+  "<img src='images/Harriet.jpg' width='100%' alt='My cat Harriet snoozing while cuddling a PlayStation controller.'/><br>" + 
+  "<p>She was named after Harriet the Spy.</p>",
+
+  "I am a video game enthusiast and enjoy exploring different genres.",
+
+  "I have a green thumb… but only for plants. Flowers and I are not on speaking terms.",
+
+  "<p>Meet Skittles!</p><br>" +
+  "<img src='images/skits.jpg' width='100%' alt='My dog sitting in the driver seat of my car.'/><br>" +
+  "<p>My son named him after his favorite candy.</p>",
+
+  "I can still pull off the classic “worm” from the breakdancing era.",
+
+  "I restored my childhood SNES by taking it apart and rebuilding it."
 ];
 const funfactId = document.getElementById("fun-fact");
 let counter = 0;
@@ -24,4 +41,4 @@ function cycleFactsArr(array) {
     }
 cycleFactsArr(funFactsArr)
 setInterval (( ) => {
-cycleFactsArr(funFactsArr)}, 20000);
+cycleFactsArr(funFactsArr)}, 10000);
